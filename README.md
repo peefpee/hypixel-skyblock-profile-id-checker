7e7094bc7c36afb91bfd75d25292448f0d638850
bearbearteam <77270434+bearbearteam@users.noreply.github.com>
2021-03-19 18:14:21 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# hypixel-skyblock-profile-id-checker
-a python tool to check your hypixel skyblock profile id
+hi, welcome to use my new hypixel skyblock profile id checker, it is not finished yet, it is still in beta version and it have alots of bug,so i will fix it asap
+thanks for using it :)
+please subscribe to bear bear team on youtube
+i welcome donations on hypixel skyblock , please add me on discord the guy who is from hypixel#0001
---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import json
+print ('welcome to the profile id checker by bear bear team *for hypixel skyblock')
+name = input('what is your ign?')
+profile = input('what is your profile name?')
+
+response = requests.get('https://sky.shiiyu.moe/api/v2/coins/'+ name + '/' + profile)
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+
+dictionary =  (response.json())
+  
+
+json_object = json.dumps(dictionary, indent = 4) 
+  
+
+with open("api.json", "w") as outfile: 
+    outfile.write(json_object) 
+
+
+    f = open('api.json',) 
+  
+
+data = json.load(f) 
+  
+
+for i in data['profile_id']: 
+    print(i) 
+  
+
+f = open("output.txt", "a")
+f.write(i)
+f.close()
+print (i)
