efdbcb84f8f7f9cda08c60dccc0b163174af5d40
bearbearteam <77270434+bearbearteam@users.noreply.github.com>
2021-03-22 10:13:06 +0800

    fix main.py not outputting right things
    fix the last line for the output.txt

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ profile = input('what is your profile name?')
 response = requests.get('https://sky.shiiyu.moe/api/v2/coins/'+ name + '/' + profile)
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
+  
 
 
 
@@ -20,7 +20,6 @@ json_object = json.dumps(dictionary, indent = 4)
 with open("api.json", "w") as outfile: 
     outfile.write(json_object) 
 
-
     f = open('api.json',) 
   
 
@@ -29,8 +28,8 @@ data = json.load(f)
 
 for i in data['profile_id']: 
     print(i) 
-  
+    f = open("output.txt", "a")
+    f.write(i)
+    f.close()
 
-f = open("output.txt", "a")
-f.write(i)
-f.close()
+
